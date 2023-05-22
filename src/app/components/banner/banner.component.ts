4ce6d2067ea7b87651359fb684befa481ef9d48a
Nguyen Trung Nhan <nhanntdl98@gmail.com>
2023-05-22 15:27:01 +0700

    update home screen

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, Input, ViewChild, ViewEncapsulation } from '@angular/core';
 import { SwiperComponent } from "swiper/angular";
 // import Swiper core and required modules
 import SwiperCore, { Swiper, Virtual } from 'swiper';
@@ -13,19 +13,12 @@ SwiperCore.use([Virtual]);
   styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent {
-  banner = <any>[
-    {
-      image: 'https://tudip.com/wp-content/uploads/2021/12/Code-Optimization-Techniques-in-Angular-website-1900x600.jpg'
-    },
-    {
-      image: 'https://kinsta.com/wp-content/uploads/2017/05/how-to-optimize-images-for-web-and-performance-1200x675.jpg'
-    }
-  ]
+  @Input() banner = <any>[]
   @ViewChild('swiper', { static: false }) swiper?: SwiperComponent;
-  slideNext(){
+  slideNext() {
     this.swiper?.swiperRef.slideNext(100);
   }
-  slidePrev(){
+  slidePrev() {
     this.swiper?.swiperRef.slidePrev(100);
   }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  bannerData = <any>[]
+  ngOnInit(): void {
+    this.bannerData = [
+      {
+        image: 'https://scontent.fsgn4-1.fna.fbcdn.net/v/t39.30808-6/242217600_1629658417237367_7562078524557990886_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=e3f864&_nc_ohc=rJ0yMNgC2lgAX8lL5lM&_nc_ht=scontent.fsgn4-1.fna&oh=00_AfBKOxVhiGyhxw5uKyeF5TDa4PHrfiuZ3UqgBz-wRHi-rg&oe=646FCE69'
+      },
+      {
+        image: 'https://scontent.fsgn13-4.fna.fbcdn.net/v/t1.6435-9/187851073_2620591208233639_8461803654191962694_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=0debeb&_nc_ohc=1Fbti2wHXkUAX866qfK&_nc_ht=scontent.fsgn13-4.fna&oh=00_AfAzGqvGR2zw6prfyLL7qNy_W9-pv37YXOtI8bFO5MRtbw&oe=64928F85'
+      }
+    ]
+  }
 }
